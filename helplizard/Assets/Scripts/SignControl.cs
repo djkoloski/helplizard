f3b00f16cb8f3ed0a98a211d3c16d95bf73f9d90
Leah Magrane <lmagrane28@gmail.com>
2016-09-18 17:33:02 -0400

    Added Signs that spawn some UI

---
+++ helplizard/Assets/Scripts/SignControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SignControl : MonoBehaviour, IPointerClickHandler {
+
+	public Canvas canvas;
+	public GameObject guiSign;
+	public string signText;
+
+	public void OnPointerClick(PointerEventData data)
+	{
+		GameObject gui = (GameObject)Instantiate(guiSign);
+		gui.transform.SetParent(canvas.transform);
+		RectTransform rect = gui.GetComponent<RectTransform>();
+		rect.offsetMax = new Vector2(0, 0);
+		rect.offsetMin = new Vector2(0, 0);
+		Text text = gui.transform.GetChild(0).GetComponent<Text>();
+		text.text = signText;
+	}
+}
