ff98f9e906f46bcf344db95d36fbce1cb50283f8
Leah Magrane <lmagrane28@gmail.com>
2016-09-20 14:46:11 -0400

    Added a basic platform move, added the drawstring gizmo util

---
+++ helplizard/Assets/Scripts/GUIUtil.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public static class GUIUtil
+{
+	public static void DrawString(string text, Vector3 worldPos, Color color)
+	{
+#if UNITY_EDITOR
+		Vector3 screenPos = UnityEditor.SceneView.lastActiveSceneView.camera.WorldToScreenPoint(worldPos);
+		if (screenPos.z < 0)
+			return;
+
+		Vector3 offset = UnityEditor.SceneView.lastActiveSceneView.camera.transform.right * 0.5f + Vector3.up * 0.25f;
+		UnityEditor.Handles.BeginGUI();
+		GUIStyle style = new GUIStyle(GUI.skin.label);
+		style.normal.textColor = color;
+		UnityEditor.Handles.Label(worldPos + offset, text, style);
+		UnityEditor.Handles.EndGUI();
+#endif
+	}
+}
---
+++ helplizard/Assets/Scripts/PlatformSwitcher.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class PlatformSwitcher : MonoBehaviour, IPointerClickHandler {
+
+	public GameObject platform;
+
+	public void OnPointerClick(PointerEventData data)
+	{
+		StartCoroutine(MoveOverSeconds(platform, this.transform.position, 2f));
+	}
+
+	public IEnumerator MoveOverSpeed(GameObject platform, Vector3 end, float speed)
+	{
+		// speed should be 1 unit per second
+		while (platform.transform.position != end)
+		{
+			platform.transform.position = Vector3.MoveTowards(platform.transform.position, end, speed * Time.deltaTime);
+			yield return new WaitForEndOfFrame();
+		}
+	}
+
+	public IEnumerator MoveOverSeconds(GameObject platform, Vector3 end, float seconds)
+	{
+		float elapsedTime = 0;
+		Vector3 startingPos = platform.transform.position;
+		while (elapsedTime < seconds)
+		{
+			platform.transform.position = Vector3.Lerp(startingPos, end, (elapsedTime / seconds));
+			elapsedTime += Time.deltaTime;
+			yield return new WaitForEndOfFrame();
+		}
+		transform.position = end;
+	}
+
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine(transform.position, platform.transform.position);
+	}
+}
