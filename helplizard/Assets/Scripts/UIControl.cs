48e39de08dd4745f94e43fb697e9c56adae2d056
Leah Magrane <lmagrane28@gmail.com>
2016-09-20 22:58:57 -0400

    added more anims

--- helplizard/Assets/Scripts/UIControl.cs
+++ helplizard/Assets/Scripts/UIControl.cs
@@ -4,7 +4,18 @@ using UnityEngine.EventSystems;
 
 public class UIControl : MonoBehaviour, IPointerClickHandler {
 
+	private Animator _animator;
+
+	void Awake()
+	{
+		_animator = GetComponent<Animator>();
+	}
 	public void OnPointerClick(PointerEventData eventData)
+	{
+		_animator.Play("sign_exit");
+	}
+
+	public void Exit()
 	{
 		Destroy(gameObject);
 	}
